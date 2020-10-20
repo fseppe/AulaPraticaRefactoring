85f15856fcaced8eb37e57f167761799fdb94250
Felipe Seppe <felipe.seppe@hotmail.com>
2020-10-20 12:59:46 -0300

    Commit 17

--- TextStatement.java
+++ TextStatement.java
@@ -1,22 +1,37 @@
 import java.util.Enumeration;
 
 public class TextStatement extends Statement {
+
     public String value(Customer aCustomer) {
         Enumeration rentals = aCustomer.getRentals();
-        String result = "Rental Record for " + aCustomer.getName() +
-        "\n";
+        String result = makeCustomerMessage(aCustomer);
         while (rentals.hasMoreElements()) {
            Rental each = (Rental) rentals.nextElement();
-           //show figures for this rental
-           result += "\t" + each.getMovie().getTitle()+ "\t" +
-           String.valueOf(each.getCharge()) + "\n";
+           result += makeRentalMessage(each);
         }
-        //add footer lines
-        result += "Amount owed is " +
-        String.valueOf(aCustomer.getTotalCharge()) + "\n";
-        result += "You earned " +
-        String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-        " frequent renter points";
+        result += makeTotalChargeMessage(aCustomer);
+        result += makeTotalFrequentRenterPointsMessage(aCustomer);
         return result;
-     }
+    }
+
+    public String makeCustomerMessage(Customer aCustomer) {
+        return "Rental Record for " + aCustomer.getName() + "\n";
+    }
+
+    public String makeRentalMessage(Rental rental) {
+        return "\t" + rental.getMovie().getTitle()+ "\t" + 
+            String.valueOf(rental.getCharge()) + "\n";
+    }
+
+    public String makeTotalChargeMessage(Customer aCustomer) {
+        return "Amount owed is " + 
+            String.valueOf(aCustomer.getTotalCharge()) + "\n";
+    }
+
+    public String makeTotalFrequentRenterPointsMessage(Customer aCustomer) {
+        return "You earned " +
+            String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
+            " frequent renter points";
+    }
+
 }
